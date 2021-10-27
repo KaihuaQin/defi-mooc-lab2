4246fcea82dcd0331fc21d96cf7d3584e1da1693
littleRound <xiaoyuanliu@berkeley.edu>
2021-10-27 04:31:52 -0700

    add profit.txt output for autograding; bugfix in filtering liquidation transaction; bugfix in assertion for unexpected liquidation

--- test/liquidation.js
+++ test/liquidation.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { network, ethers } = require("hardhat");
 const { BigNumber, utils }  = require("ethers");
+const { writeFile } = require('fs');
 
 describe("Liquidation", function () {
   it("test", async function () {
@@ -35,10 +36,10 @@ describe("Liquidation", function () {
         v => v && v.topics && v.address === '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9' && Array.isArray(v.topics) && 
         v.topics.length > 3 && v.topics[0] === '0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286')
 
-    const expectedLiquidationEvents = liquidationReceipt.logs.filter(v => v.topics[3] === '0x59CE4a2AC5bC3f5F225439B2993b86B42f6d3e9F');
+    const expectedLiquidationEvents = liquidationReceipt.logs.filter(v => v.topics[3] === '0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f');
 
     expect(expectedLiquidationEvents.length, "no expected liquidation").to.be.above(0);
-    expect(liquidationEvents.length, "unexpected liquidation").to.be.above(expectedLiquidationEvents.length);
+    expect(liquidationEvents.length, "unexpected liquidation").to.be.equal(expectedLiquidationEvents.length);
 
     const afterLiquidationBalance = BigNumber.from(await hre.network.provider.request({
         method: "eth_getBalance",
@@ -47,6 +48,8 @@ describe("Liquidation", function () {
 
     const profit = afterLiquidationBalance.sub(beforeLiquidationBalance);
     console.log("Profit", utils.formatEther(profit), "ETH");
+
+    writeFile('profit.txt', String(utils.formatEther(profit)), function (err) {console.log("failed to write profit.txt: %s", err)});
     expect(profit.gt(BigNumber.from(0)), "not profitable").to.be.true;
   });
 });
